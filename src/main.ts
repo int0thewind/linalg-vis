5b2bf0d807391633ff0a98f52a8ca3d1f3b7932b
Hanzhi Yin <yinhanzhi@gmail.com>
2020-04-11 21:37:32 -0500

    add: page now fetch latest github commit

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,16 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const github = document.querySelector('p.description#github');
+
+// Fetch GitHub info
+fetch('https://api.github.com/repos/int0thewind/linalg-vis/commits')
+  .then(d => d.json())
+  .then((req) => {
+    const commit = req[0].sha;
+    const link = req[0].html_url;
+    const date = req[0].commit.committer.date.substring(0, 10);
+    github.innerHTML = `Latest Commit: <a href=${link} target="_blank">${commit.substring(0, 6)}</a> on ${date}`;
+  })
+  .catch((e) => { console.error(e); github.innerHTML = 'Failed to fetch latest commit history'; });
