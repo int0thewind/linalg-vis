e3a66761c5c83ae7390da271d7bbd9f3d1e28623
Hanzhi Yin <yinhanzhi@gmail.com>
2020-05-26 23:42:55 -0500

    init base vector set

--- src/app/matrix-board/matrix-board.component.ts
+++ src/app/matrix-board/matrix-board.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import * as math from 'mathjs';
 import * as d3 from 'd3';
+import { Shape } from '../shape';
+import { Vector } from '../vector';
 
 @Component({
   selector: 'app-matrix-board',
@@ -15,16 +17,21 @@ export class MatrixBoardComponent implements OnInit, OnChanges {
 
   margin = {top: 20, bottom: 20, right: 20, left: 20};
 
-  xScale = null;
+  xScale: d3.ScaleLinear<number, number> = null;
 
-  yScale = null;
+  yScale: d3.ScaleLinear<number, number> = null;
 
-  listOfObject = [];
+  listOfShape: Shape[] = [];
+
+  xBaseVectorId = 'x-base-vector';
+
+  yBaseVectorId = 'y-base-vector';
 
   constructor() {}
 
   ngOnInit(): void {
     this.initCoordinate();
+    this.initBaseVectors();
   }
 
   initCoordinate(): void {
@@ -46,7 +53,7 @@ export class MatrixBoardComponent implements OnInit, OnChanges {
     document.querySelectorAll('g.tick > text').forEach((textElem) => {
       if (textElem.innerHTML === '0') {
         // Dont know why angular complains about the style property. It is actually work.
-        textElem.style.visibility = 'hidden';
+        // textElem.style.visibility = 'hidden';
       }
     });
   }
@@ -60,14 +67,47 @@ export class MatrixBoardComponent implements OnInit, OnChanges {
     // We do not reset the xScale and yScale here.
   }
 
+  updateCoordinate(): void {
+    this.removeCoordinate();
+    this.initCoordinate();
+  }
+
+  initBaseVectors(): void {
+    const xBaseVector = new Vector(this.xBaseVectorId, 'red', 0, 0, this.mat.get([0, 0]), this.mat.get([0, 1]));
+    const yBaseVector = new Vector(this.yBaseVectorId, 'blue', 0, 0, this.mat.get([1, 0]), this.mat.get([1, 1]));
+    this.listOfShape.push(xBaseVector);
+    this.listOfShape.push(yBaseVector);
+    this.renderShapes();
+  }
+
+  removeBaseVectors(): void {
+    this.removeShape(this.xBaseVectorId);
+    this.removeShape(this.yBaseVectorId);
+  }
+
+  removeShape(id: string): void {
+    this.listOfShape = this.listOfShape.filter((shape) => {
+      if (shape.id === id) {
+        shape.remove();
+      }
+      return shape.id !== id;
+    });
+  }
+
+  renderShapes(): void {
+    const svg = d3.select('svg.board');
+    for (const shape of this.listOfShape) {
+      shape.render(svg, this.xScale, this.yScale);
+    }
+  }
+
   getDet(): number {
     return math.det(this.mat);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.range) {
-      this.removeCoordinate();
-      this.initCoordinate();
+      this.updateCoordinate();
     }
   }
 }
