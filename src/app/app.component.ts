8b75119bcbda6677b5ca2d064830b17961d70eb6
Hanzhi Yin <yinhanzhi@gmail.com>
2020-05-30 09:51:41 -0500

    Change way of rendering elements for main app Switch from traditional DOM rendering to the Angualr way of rendering elements.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { version as d3version } from 'd3';
+import { VERSION as angularVersion } from '@angular/core';
+import { version as mathjsVersion } from 'mathjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'linalg-vis';
+
+  githubInfo = {
+    isValid: false,
+    link: '',
+    commit: '',
+    date: '',
+  };
+
+  d3VersionInfo = d3version;
+
+  mathJsVersionInfo = mathjsVersion;
+
+  angularVersionInfo = angularVersion.full;
+
+  readonly githubLink = 'https://api.github.com/repos/int0thewind/linalg-vis/commits';
+
+  ngOnInit(): void {
+    fetch(this.githubLink)
+      .then(d => d.json())
+      .then((req) => {
+        this.githubInfo.commit = req[0].sha.substring(0, 6);
+        this.githubInfo.link = req[0].html_url;
+        this.githubInfo.date = req[0].commit.committer.date.substring(0, 10);
+        this.githubInfo.isValid = true;
+      })
+      .catch(e => console.error(e));
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { version as d3version } from 'd3';
-import { VERSION as angularVersion } from '@angular/core';
-import { version as mathjsVersion } from 'mathjs';
-
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-// Fetch install info
-const version = document.querySelector('p.desc#version');
-version.innerHTML = `Built with <a href='https://d3js.org'>D3.js ${d3version}</a>, <a href='https://mathjs.org'>mathJS ${mathjsVersion}</a>, and <a href='https://angular.io'>Angular ${angularVersion.full}</a>`;
-
-// Fetch GitHub info
-const github = document.querySelector('p.desc#github');
-
-fetch('https://api.github.com/repos/int0thewind/linalg-vis/commits')
-  .then(d => d.json())
-  .then((req) => {
-    const commit = req[0].sha;
-    const link = req[0].html_url;
-    const date = req[0].commit.committer.date.substring(0, 10);
-    github.innerHTML = `Latest Commit: <a href=${link} target="_blank">${commit.substring(0, 6)}</a> on ${date}`;
-  })
-  .catch((e) => { console.error(e); github.innerHTML = 'Failed to fetch latest commit history'; });
