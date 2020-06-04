ce0ea061ba2f166a5c5a512d1da3974496fc5893
Hanzhi Yin <yinhanzhi@gmail.com>
2020-06-03 20:15:52 -0500

    remove excess class variables

--- src/app/matrix-board-control/matrix-board-control.component.ts
+++ src/app/matrix-board-control/matrix-board-control.component.ts
@@ -4,30 +4,19 @@ import { Matrix, det, eigs, matrix } from 'mathjs';
 import { MatrixBoardDataService } from '../matrix-board-data.service';
 import * as d3 from 'd3';
 
-export type shapeStr = 'Vector' | 'Dot' | 'Ellipse' | 'Rectangle' | 'Polygon';
-
 @Component({
   selector: 'app-matrix-board-control',
   templateUrl: './matrix-board-control.component.html',
   styleUrls: ['./matrix-board-control.component.css']
 })
 export class MatrixBoardControlComponent implements OnInit {
-
-  objectKeys = Object.keys; // alias of object keys to retrieve an key of an object;
-
   matrix: Matrix;
 
   shapeList: Shape[];
 
-  currType: shapeStr = 'Vector';
+  readonly acceptedShapes = ['Vector', 'Dot', 'Ellipse', 'Rectangle', 'Polygon'];
 
-  readonly acceptedShapes = {
-    Vector,
-    Dot,
-    Ellipse,
-    Rectangle,
-    Polygon,
-  };
+  currType = 'Vector';
 
   xScale = d3.scaleLinear();
 
